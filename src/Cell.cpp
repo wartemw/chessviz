14db1f4987da73fe0f3a1d2cb5205f3d7bfb5013
wartemw <wartemw@gmail.com>
2019-04-03 12:48:45 +0700

    Create cell class

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -1,5 +1,55 @@
+// Created by wartemw on 06.03.19.
 //
-// Created by wartemw on 02.04.19.
-//
+#include <iostream>
 
-#include "Cell.h"
+using namespace std;
+
+enum Color { BLACK, WHITE };
+
+enum Chessman { KING, QUEEN, ROOK, K_NIGHT, BISHOP, PAWN, NON };
+
+class Cell {
+private:
+    Chessman chessman;
+    Color colorChessman;
+
+public:
+    Cell() : chessman(NON), colorChessman(BLACK)
+    {
+    }
+
+    Cell(Chessman chessman) : chessman(chessman), colorChessman(BLACK)
+    {
+    }
+
+    Cell(Chessman chessman, Color colorChessman)
+        : chessman(chessman), colorChessman(colorChessman)
+    {
+    }
+
+    friend ostream& operator<<(ostream& os, const Cell& cell)
+    {
+        os << cell.designation();
+        return os;
+    }
+
+    char designation() const
+    {
+        switch (chessman) {
+        case KING:
+            return colorChessman == WHITE ? 'K' : 'k';
+        case QUEEN:
+            return colorChessman == WHITE ? 'Q' : 'q';
+        case ROOK:
+            return colorChessman == WHITE ? 'R' : 'r';
+        case K_NIGHT:
+            return colorChessman == WHITE ? 'N' : 'n';
+        case BISHOP:
+            return colorChessman == WHITE ? 'B' : 'b';
+        case PAWN:
+            return colorChessman == WHITE ? 'P' : 'p';
+        default:
+            return '_';
+        }
+    }
+};
