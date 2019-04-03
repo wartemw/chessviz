4319b312cedf0d693faa7adad238844fc391e08e
wartemw <wartemw@gmail.com>
2019-04-03 22:27:01 +0700

    reformat code style

--- src/Board.h
+++ src/Board.h
@@ -5,17 +5,11 @@
 #ifndef CHESSVIZ_BOARD_H
 #define CHESSVIZ_BOARD_H
 
-#include <string>
 #include "Cell.h"
+#include <ostream>
+#include <string>
+
 using namespace std;
-
-enum Color {
-    BLACK, WHITE
-};
-
-enum Chessman {
-    KING, QUEEN, ROOK, K_NIGHT, BISHOP, PAWN, NON
-};
 
 struct MoveChessman {
     string move;
@@ -31,79 +25,21 @@ struct MoveChessman {
     bool castling;
 };
 
-
-
 class Board {
-
 private:
     static const int NUMBER_OF_CELLS = 8;
     Cell cells[NUMBER_OF_CELLS][NUMBER_OF_CELLS];
+
 public:
-    Board() {
-        setDefault();
-    }
+    Board();
 
-    friend ostream &operator<<(ostream &os, const Board &board) {
-        for (int i = NUMBER_OF_CELLS - 1; i >= 0; --i) {
-            os << i + 1 << ' ';
-            for (int j = 0; j < NUMBER_OF_CELLS; ++j) {
-                os << board.cells[i][j] << ' ';
-            }
-            os << '\n';
-        }
-        os << "  ";
-        for (char i = 'a'; i <= 'h'; ++i)
-            os << i << ' ';
-        return os;
-    }
+    friend ostream& operator<<(ostream& os, const Board& board);
 
-    void setDefault() {
-        cells[0][0] = Cell(ROOK, WHITE);
-        cells[0][1] = Cell(K_NIGHT, WHITE);
-        cells[0][2] = Cell(BISHOP, WHITE);
-        cells[0][3] = Cell(QUEEN, WHITE);
-        cells[0][4] = Cell(KING, WHITE);
-        cells[0][5] = Cell(BISHOP, WHITE);
-        cells[0][6] = Cell(K_NIGHT, WHITE);
-        cells[0][7] = Cell(ROOK, WHITE);
-        for (int i = 0; i < NUMBER_OF_CELLS; ++i)
-            cells[1][i] = Cell(PAWN, WHITE);
+    void setDefault();
 
-        cells[7][0] = Cell(ROOK);
-        cells[7][1] = Cell(K_NIGHT);
-        cells[7][2] = Cell(BISHOP);
-        cells[7][3] = Cell(QUEEN);
-        cells[7][4] = Cell(KING);
-        cells[7][5] = Cell(BISHOP);
-        cells[7][6] = Cell(K_NIGHT);
-        cells[7][7] = Cell(ROOK);
-        for (int i = 0; i < NUMBER_OF_CELLS; ++i)
-            cells[6][i] = Cell(PAWN);
-    }
+    int changeNumb(char letter);
 
-    int changeNumb(char letter) {
-        if ('0' < letter && letter < '9')
-            return letter - '0' - 1;
-        if ('a' < letter && letter < 'z')
-            return letter - 'a';
-        return letter;
-    }
-
-    void moveChessman(MoveChessman &chessman) {
-        //cout << endl << changeNumb(chessman.firstRow) << " | " << changeNumb(chessman.firstCol) << endl;
-        //cout << changeNumb(chessman.endRow) << " | " << changeNumb(chessman.endCol) << endl << endl;
-        if(chessman.type == '-') {
-            Cell temp = cells[changeNumb(chessman.firstCol)][changeNumb(chessman.firstRow)];
-            cells[changeNumb(chessman.firstCol)][changeNumb(chessman.firstRow)] =
-                    cells[changeNumb(chessman.endCol)][changeNumb(chessman.endRow)];
-            cells[changeNumb(chessman.endCol)][changeNumb(chessman.endRow)] = temp;
-        }else {
-            cells[changeNumb(chessman.endCol)][changeNumb(chessman.endRow)] =
-                    cells[changeNumb(chessman.firstCol)][changeNumb(chessman.firstRow)];
-            Cell cell;
-            cells[changeNumb(chessman.firstCol)][changeNumb(chessman.firstRow)] = cell;
-        }
-    }
+    void moveChessman(MoveChessman& chessman);
 };
 
-#endif //CHESSVIZ_BOARD_H
+#endif // CHESSVIZ_BOARD_H
--- src/Cell.cpp
+++ src/Cell.cpp
@@ -4,33 +4,41 @@
 
 #include "Cell.h"
 
-Cell::Cell() : chessman(NON), colorChessman(BLACK) {}
+Cell::Cell() : chessman(NON), colorChessman(BLACK)
+{
+}
 
-Cell::Cell(Chessman chessman) : chessman(chessman), colorChessman(BLACK) {}
+Cell::Cell(Chessman chessman) : chessman(chessman), colorChessman(BLACK)
+{
+}
 
 Cell::Cell(Chessman chessman, Color colorChessman)
-        : chessman(chessman), colorChessman(colorChessman) {}
+    : chessman(chessman), colorChessman(colorChessman)
+{
+}
 
-ostream &operator<<(ostream &os, const Cell &cell) {
+ostream& operator<<(ostream& os, const Cell& cell)
+{
     os << cell.designation();
     return os;
 }
 
-char Cell::designation() const {
+char Cell::designation() const
+{
     switch (chessman) {
-        case KING:
-            return colorChessman == WHITE ? 'K' : 'k';
-        case QUEEN:
-            return colorChessman == WHITE ? 'Q' : 'q';
-        case ROOK:
-            return colorChessman == WHITE ? 'R' : 'r';
-        case K_NIGHT:
-            return colorChessman == WHITE ? 'N' : 'n';
-        case BISHOP:
-            return colorChessman == WHITE ? 'B' : 'b';
-        case PAWN:
-            return colorChessman == WHITE ? 'P' : 'p';
-        default:
-            return '_';
+    case KING:
+        return colorChessman == WHITE ? 'K' : 'k';
+    case QUEEN:
+        return colorChessman == WHITE ? 'Q' : 'q';
+    case ROOK:
+        return colorChessman == WHITE ? 'R' : 'r';
+    case K_NIGHT:
+        return colorChessman == WHITE ? 'N' : 'n';
+    case BISHOP:
+        return colorChessman == WHITE ? 'B' : 'b';
+    case PAWN:
+        return colorChessman == WHITE ? 'P' : 'p';
+    default:
+        return '_';
     }
 }
