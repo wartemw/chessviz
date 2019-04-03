1ccdd64bfacd830151f87484e9c1548f3916c457
wartemw <wartemw@gmail.com>
2019-04-03 08:46:54 +0700

    split declarations and implementations of the cell class

--- src/Cell.h
+++ src/Cell.h
@@ -5,6 +5,7 @@
 #ifndef CHESSVIZ_CELL_H
 #define CHESSVIZ_CELL_H
 
+#include <ostream>
 #include "Board.h"
 
 
@@ -13,37 +14,15 @@ private:
     Chessman chessman;
     Color colorChessman;
 public:
-    Cell() : chessman(NON), colorChessman(BLACK) {}
+    Cell();
 
-    Cell(Chessman chessman) : chessman(chessman), colorChessman(BLACK) {}
+    Cell(Chessman chessman);
 
-    Cell(Chessman chessman, Color colorChessman) : chessman(chessman), colorChessman(colorChessman) {}
+    Cell(Chessman chessman, Color colorChessman);
 
-    friend ostream &operator<<(ostream &os, const Cell &cell) {
-        os << cell.designation();
-        return os;
-    }
+    char designation() const;
 
-
-    char designation() const {
-        switch (chessman) {
-            case KING:
-                return colorChessman == WHITE ? 'K' : 'k';
-            case QUEEN:
-                return colorChessman == WHITE ? 'Q' : 'q';
-            case ROOK:
-                return colorChessman == WHITE ? 'R' : 'r';
-            case K_NIGHT:
-                return colorChessman == WHITE ? 'N' : 'n';
-            case BISHOP:
-                return colorChessman == WHITE ? 'B' : 'b';
-            case PAWN:
-                return colorChessman == WHITE ? 'P' : 'p';
-            default:
-                return '_';
-        }
-    }
+    friend ostream &operator<<(ostream &os, const Cell &cell);
 };
-
 
 #endif //CHESSVIZ_CELL_H
